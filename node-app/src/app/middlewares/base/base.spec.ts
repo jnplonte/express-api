514fe080844ed9aa6fc6f0b7e4ce033771ad40f4
John Paul Onte <jnpl.onte@gmail.com>
2021-04-06 15:43:00 +0800

    update test lint

--- node-app/src/app/middlewares/base/base.spec.ts
+++ node-app/src/app/middlewares/base/base.spec.ts
@@ -1,28 +1,27 @@
 import { expect } from 'chai';
 import { BaseMiddleware } from './base.middleware';
 
-
 describe('base middleware', () => {
-    let middlewares: any;
+	let middlewares: any;
 
-    before( (done) => {
-        middlewares = new BaseMiddleware('sandbox');
+	before((done) => {
+		middlewares = new BaseMiddleware('sandbox');
 
-        done();
-    });
+		done();
+	});
 
-    it('should have the basic method', (done) => {
-        expect(middlewares.middleWare).to.exist;
-        expect(middlewares.middleWare).to.be.a('function');
-        expect(middlewares.registerServices).to.exist;
-        expect(middlewares.registerServices).to.be.a('function');
+	it('should have the basic method', (done) => {
+		expect(middlewares.middleWare).to.exist;
+		expect(middlewares.middleWare).to.be.a('function');
+		expect(middlewares.registerServices).to.exist;
+		expect(middlewares.registerServices).to.be.a('function');
 
-        done();
-    });
+		done();
+	});
 
-    it('should get empty value', (done) => {
-        expect(Object.keys(middlewares.services).length).to.equal(0);
+	it('should get empty value', (done) => {
+		expect(Object.keys(middlewares.services).length).to.equal(0);
 
-        done();
-    });
+		done();
+	});
 });
--- node-app/src/app/middlewares/core/core.middleware.ts
+++ node-app/src/app/middlewares/core/core.middleware.ts
@@ -2,103 +2,93 @@ import { Request, Response } from 'express';
 import { BaseMiddleware } from '../base/base.middleware';
 
 export class CoreMiddleware extends BaseMiddleware {
-    constructor(app, cache?) {
-        super(app, cache);
-    }
+	constructor(app, cache?) {
+		super(app, cache);
+	}
 
-    get attributes(): any {
-        return [];
-    }
+	get attributes(): any {
+		return [];
+	}
 
-    get requiredParameters(): string[] {
-        return [];
-    }
+	get requiredParameters(): string[] {
+		return [];
+	}
 
-    all(req: Request, res: Response): void {
+	all(req: Request, res: Response): void {}
 
-    }
+	get(req: Request, res: Response): void {}
 
-    get(req: Request, res: Response): void {
+	post(req: Request, res: Response): void {}
 
-    }
+	put(req: Request, res: Response): void {}
 
-    post(req: Request, res: Response): void {
+	delete(req: Request, res: Response): void {}
 
-    }
+	protected getInclude(model?: any): Array<any> {
+		return [];
+	}
 
-    put(req: Request, res: Response): void {
+	protected postInclude(model?: any): Array<any> {
+		return [];
+	}
 
-    }
+	protected autoFillPostInformation(data: object = {}, authData: object = {}): object {
+		if (typeof authData['id'] !== 'undefined') {
+			data['createdUserId'] = authData['id'];
+			data['updatedUserId'] = authData['id'];
+		}
 
-    delete(req: Request, res: Response): void {
+		data['createdAt'] = new Date();
+		data['updatedAt'] = new Date();
 
-    }
+		return data;
+	}
 
-    protected getInclude(model?: any): Array<any> {
-        return [];
-    }
+	protected autoFillAllPostInformation(data: Array<object>, authData: object = {}): Array<object> {
+		return data.map((dataval) => {
+			if (typeof authData['id'] !== 'undefined') {
+				dataval['createdUserId'] = authData['id'];
+				dataval['updatedUserId'] = authData['id'];
+			}
 
-    protected postInclude(model?: any): Array<any> {
-        return [];
-    }
+			dataval['createdAt'] = new Date();
+			dataval['updatedAt'] = new Date();
 
-    protected autoFillPostInformation(data: object = {}, authData: object = {}): object {
-        if (typeof(authData['id']) !== 'undefined') {
-            data['createdUserId'] = authData['id'];
-            data['updatedUserId'] = authData['id'];
-        }
+			return dataval;
+		});
+	}
 
-        data['createdAt'] = new Date();
-        data['updatedAt'] = new Date();
+	protected autoFillPutInformation(data: object = {}, authData: object = {}): object {
+		if (typeof authData['id'] !== 'undefined') {
+			data['updatedUserId'] = authData['id'];
+		}
 
-        return data;
-    }
+		data['updatedAt'] = new Date();
 
-    protected autoFillAllPostInformation(data: Array<object>, authData: object = {}): Array<object> {
-        return data.map( (dataval) => {
-            if (typeof(authData['id']) !== 'undefined') {
-                dataval['createdUserId'] = authData['id'];
-                dataval['updatedUserId'] = authData['id'];
-            }
+		return data;
+	}
 
-            dataval['createdAt'] = new Date();
-            dataval['updatedAt'] = new Date();
+	protected autoFillAllPutInformation(data: Array<object>, authData: object = {}): Array<object> {
+		return data.map((dataval) => {
+			if (typeof authData['id'] !== 'undefined') {
+				dataval['updatedUserId'] = authData['id'];
+			}
 
-            return dataval;
-        });
-    }
+			dataval['updatedAt'] = new Date();
 
-    protected autoFillPutInformation(data: object = {}, authData: object = {}): object {
-        if (typeof(authData['id']) !== 'undefined') {
-            data['updatedUserId'] = authData['id'];
-        }
+			return dataval;
+		});
+	}
 
-        data['updatedAt'] = new Date();
+	protected isAdmin(authData: object = {}): boolean {
+		return authData['roleId'] && authData['roleId'] === 1;
+	}
 
-        return data;
-    }
-
-    protected autoFillAllPutInformation(data: Array<object>, authData: object = {}): Array<object> {
-        return data.map( (dataval) => {
-            if (typeof(authData['id']) !== 'undefined') {
-                dataval['updatedUserId'] = authData['id'];
-            }
-
-            dataval['updatedAt'] = new Date();
-
-            return dataval;
-        });
-    }
-
-    protected isAdmin(authData: object = {}): boolean {
-        return (authData['roleId'] && authData['roleId'] === 1);
-    }
-
-    protected cacheCheck(pageCache?, nameCache?) {
-        if (pageCache && nameCache) {
-            return (callback) => pageCache.del(nameCache, () => callback);
-        } else {
-            return (callback) => callback;
-        }
-    }
+	protected cacheCheck(pageCache?, nameCache?) {
+		if (pageCache && nameCache) {
+			return (callback) => pageCache.del(nameCache, () => callback);
+		} else {
+			return (callback) => callback;
+		}
+	}
 }
